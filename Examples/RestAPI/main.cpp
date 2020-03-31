b7634d65fcf503644daa12db78abe254d80776d3
Mauro Marssola <mauro.marssola@hotmail.com>
2020-03-31 09:48:33 -0300

    update example

--- Examples/RestAPI/main.cpp
+++ Examples/RestAPI/main.cpp
@@ -9,18 +9,10 @@
 #include <ratio>
 
 void about(QExpress::Request& request, QExpress::Response& response) {
+    Q_UNUSED(request);
     response.send(QJsonObject{
                       { "server", QString(QExpress::HTTP::SERVER_VERSION) }
                   });
-    std::chrono::high_resolution_clock::time_point start = request.time();
-    std::chrono::high_resolution_clock::time_point end = response.time();
-    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-    qDebug().noquote()
-            << request.method()
-            << request.url()
-            << response.getStatus()
-            << response.getHeader("Content-Length")
-            << QString("%1ms").arg(ms);
 }
 
 int main(int argc, char *argv[])
